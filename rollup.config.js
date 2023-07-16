da40e3a0f71aa17980552bcf32a2eff38649bef7
Vinicius Egidio <me@vinicius.io>
2023-07-16 07:48:57 +0200

    ⬆️ Replaced Rollup Terser with the new community version

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import dts from 'rollup-plugin-dts';
 import json from '@rollup/plugin-json';
 import typescript from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import terser from "@rollup/plugin-terser"
 
 export default [
     {
